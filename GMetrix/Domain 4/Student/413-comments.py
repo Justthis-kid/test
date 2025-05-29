18eb027ba8a4306c0c2e7bd0bf923e970b923f17
Justthis-kid <shalom.ayoola@asmprograms.org>
2025-05-29 11:10:25 -0500

    line 10 change

--- GMetrix/Domain 4/Student/413-comments.py
+++ GMetrix/Domain 4/Student/413-comments.py
@@ -1,7 +1,7 @@
 game_state = True
 game_lives = 1
 
-
+#Prints positions achieved during lives
 while game_lives <= 3:
     for i in range(1,11): 
         print(f"You have reached position {i} in game life {game_lives}")
